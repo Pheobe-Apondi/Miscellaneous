c1cf212bb880152d4199d444c79e72d84ff8f300
Pheobe-Apondi <pheobegloriaapondi.com>
2025-03-27 13:04:12 +0300

    ARRAYS I practiced some operations you can perform on an array

---
+++ Strings.js
@@ -0,0 +1,12 @@
+//Write a program that removes all voweles from a string
+let words = "Get to know me.";
+  words = words.replace(/[aeiouAEIOU]/g,"");
+  console.log(words)
+
+let favourite = "My favourite character pinochio "
+   favourite =favourite.replace (/[aeiouAEIOU]/g,"");
+console.log(favourite)
+
+//Write a program that counts how many times a number appears in a string 
+//banana a
+let s = "Banana"
---
+++ nestedArrays.js
@@ -0,0 +1,35 @@
+//nested array is an array that contains arrays inside it
+let nestedArrays = [[10,40,50],[50,40,50],[80,546,43]]
+//index of nested arrays
+//console.log(nestedArrays[0])
+//console.log(nestedArrays[2])
+
+
+//index of elements in nested arrays
+//console.log(nestedArrays[0][2])
+//console.log(nestedArrays[2][2])
+
+//nested arrays can have different lengths
+let names = [[10,40],[40,50,60,70,40],[40,30,20]]
+//console.log(names)
+
+//multidimensional arrays- is a satructured nested arrays where all inner arrays follow a consinstent structure e.g grids and tables excel sheets with rows and columns
+//2D Dimensional arrays (rows,columns) like a spreadsheet
+let matrix = [
+   [2,4,6],
+   [3,5,4],
+   [4,5,6]
+
+]
+//console.log(matrix[1][2])
+
+//3D DImensional arrays
+// Data stored in layers like a rubic cube
+
+let cube = [
+   [[10,20],[30,40]],
+   [[30,60],[70,50]]
+]
+
+console.log(cube[1],[0],[1])   //review
+//console.log(cube[1],[2],[1])
